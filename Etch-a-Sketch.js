928f4eef4f1a2a0ec8842fbfa19732cf78e5e0da
TahjayW <tahjay.watson@gmail.com>
2024-04-06 19:45:55 -0400

    ADD gridCellDimension
    Added gridCellDimension for cell sizing
    Changed body and container dimensions

--- Etch-a-Sketch.js
+++ Etch-a-Sketch.js
@@ -6,38 +6,36 @@ function getSize() {
     }
     return gridSize;
 }
-
+//Add elements to HTML via selector
 const body = document.querySelector("body");
-
-
+//Get size from user
 const gridSize = Math.pow(getSize(), 2);
-
+//Defining cell dimensions
 const generatedGridContainer = document.createElement("div");
-const cellsPerRow = 60 / Math.sqrt(gridSize);
+const cellsPerRow = Math.sqrt(gridSize);
 generatedGridContainer.setAttribute("id", "container")
-
+const gridCellDimension = (100 / cellsPerRow) + "%";
 //Create and Populate Grid
 for (i = 0; i < gridSize; i++) {
     const gridCell = document.createElement("div");
     gridCell.id = 'div #' + i;
     gridCell.className = "liveCells";
-    gridCell.style.width = cellsPerRow + "px";
+    gridCell.style.backgroundColor = "white";
+    gridCell.style.minWidth = gridCellDimension;
+    gridCell.style.minHeight = gridCellDimension;
     generatedGridContainer.append(gridCell);
 }
-
-
-
-
+//Add grid to doc
 body.append(generatedGridContainer);
-
-
 //Bubbling to have live cells send mouse event to body, change class to deadcell
-
 generatedGridContainer.addEventListener("mouseover", (event) => {
     if (event.target.className === "liveCells") {
         event.stopPropagation();
-        event.target.style.color = 'black';
+        event.target.style.backgroundColor = 'black';
         event.target.className = "deadCells";
+        event.target.style.minWidth = gridCellDimension;
+        event.target.style.minHeight = gridCellDimension;
+        
     }
 
 });
