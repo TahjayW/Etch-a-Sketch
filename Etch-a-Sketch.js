9d0333179dd138f6f3f0e52e9da00d51b7b99e0b
TahjayW <tahjay.watson@gmail.com>
2024-04-06 11:03:12 -0400

    ADDED liveCells logic
    Added ifs for live cells to resolve issue
    (NEEDS TESTING)
    Changed generatedGrid from docfrag to div
    updates styles

--- Etch-a-Sketch.js
+++ Etch-a-Sketch.js
@@ -1,10 +1,10 @@
-function getSize(){
+function getSize() {
     const gridSize = prompt("Hey, how big do you want this thing?");
-    if(gridSize > 99){
+    if (gridSize > 99) {
         alert("That's too big");
         getSize();
     }
-return gridSize;
+    return gridSize;
 }
 
 const body = document.querySelector("body");
@@ -12,27 +12,38 @@ const body = document.querySelector("body");
 
 const gridSize = Math.pow(getSize(), 2);
 
-const generatedGrid = document.createDocumentFragment();
+const generatedGridContainer = document.createElement("div");
+generatedGridContainer.setAttribute("id", "container")
 
 //Create and Populate Grid
-for(i= 0; i< gridSize; i++){
+for (i = 0; i < gridSize; i++) {
     const gridCell = document.createElement("div");
-    gridCell.id = 'div #' + i; 
+    gridCell.id = 'div #' + i;
     gridCell.className = "liveCells";
-generatedGrid.append(gridCell);
+    generatedGridContainer.append(gridCell);
 }
 
 
 
-body.append(generatedGrid);
+body.append(generatedGridContainer);
+
+
+//Bubbling to have live cells send mouse event to body, change class to deadcell
+
+generatedGridContainer.addEventListener("mouseover", (event) => {
+    if (event.target.className = "liveCells") {
+        event.target.style.color = 'black';
+        event.target.className = "deadCells";
+    }
+
+});
 
 
 
 
 
 
-
-if(typeof module === 'object'){
+if (typeof module === 'object') {
     module.exports = getSize();
 }
 
